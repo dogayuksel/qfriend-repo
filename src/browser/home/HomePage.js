d39767f9529f352234730306ec313b6d9827e1d7
Doga Yuksel <dogayuksel@gmail.com>
2017-01-05 20:50:58 +0100

    remove new year break message

--- src/browser/home/HomePage.js
+++ src/browser/home/HomePage.js
@@ -16,23 +16,11 @@ const TonightPage = ({ queues, reportEventClick }) => (
   <View>
     <Title message="Live queue updates from famous Berlin Clubs" />
     <PageHeader
-      description={queues.size > 0
+      description={Object.keys(queues).length
                  ? 'No queueing! Go clubbing!'
-                 : 'one of our comrades was abducted by aliens.'}
-      heading={queues.size > 0 ? 'Tonight' : 'friends'}
+                 : 'No queues right now... We will be live this saturday!'}
+      heading={Object.keys(queues).length ? 'Tonight' : 'Events'}
     />
-    <Text ml={2} mb={4} style={{ maxWidth: '390px' }}>
-      We will board our spacecraft and go on a mission to retrieve&nbsp;
-      <Link
-        target="_blank"
-        onClick={() => reportEventClick('st. eggs')}
-        to={'https://soundcloud.com/st-eggs'}
-        theme="secondary"
-      >
-        St. Eggs.&nbsp;
-      </Link>
-      We hope to land back on earth by 7.1.17. Until then, check our weekly updates & write to us if you want logs of the mission. New years out.
-    </Text>
     <Block>
       <QueuesTonight />
     </Block>
