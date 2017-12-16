bf47086f4348722888f4780948dae62755c65340
Doga Yuksel <dogayuksel@gmail.com>
2017-12-16 13:41:57 +0100

    Add facebook link to footer

--- src/browser/app/Footer.js
+++ src/browser/app/Footer.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Box, Text, Link } from './components';
 import { FormattedMessage, defineMessages } from 'react-intl';
+import './footer.css';
 
 const messages = defineMessages({
   madeByHtml: {
@@ -15,13 +16,41 @@ const AppFooter = () => (
     border="top"
     marginTop={1}
     paddingVertical={1}
+    className="footer-container"
   >
-    <Text
+    <Box
       marginHorizontal={1}
-      size={-1}
+      display="flex"
+      flexWrap="wrap"
     >
-      Wait less for a mess!
-    </Text>
+      <Box>
+        <Text
+          bold
+          size={-1}
+          paddingRight={0.2}
+        >
+          Wait less
+        </Text>
+      </Box>
+      <Box>
+        <Text
+          bold
+          size={-1}
+        >
+          for a mess!
+        </Text>
+      </Box>
+    </Box>
+    <Link
+      marginHorizontal={1}
+      to="https://www.facebook.com/QFriendBerlin/"
+      className="footer-link"
+      size={-1}
+      color="primary"
+      bold
+    >
+      find us on Facebook
+    </Link>
   </Box>
 );
 
