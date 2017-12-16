cbc97a187294db4001c4fba60fcd97485c7227c1
Doga Yuksel <dogayuksel@gmail.com>
2017-12-16 03:32:38 +0100

    Refactor QueueView

--- src/browser/queue/QueueView.js
+++ src/browser/queue/QueueView.js
@@ -1,8 +1,9 @@
 /* @flow */
-import type { State } from '../../common/types';
 import React from 'react';
-import { Text, Box } from '../app/components';
 import { connect } from 'react-redux';
+
+import type { State } from '../../common/types';
+import { Text, Box } from '../app/components';
 import { checkAllQueues } from '../../common/queues/actions';
 
 const hourToMinute = 90;
@@ -12,7 +13,7 @@ const makeColor = (time) => {
   if (time < 45) return 'success';
   if (time < 90) return 'warning';
   return 'danger';
-}
+};
 
 class QueueView extends React.Component {
 
@@ -27,7 +28,7 @@ class QueueView extends React.Component {
       case 1: return 'updated a minute ago';
       default: {
         if (updateTime >= 120) {
-          return `updated ${updateTime / 60 | 0} hours ago`;
+          return `updated ${(updateTime / 60).toFixed(0)} hours ago`;
         } else if (updateTime >= 60) {
           return 'updated an hour ago';
         }
@@ -63,42 +64,43 @@ class QueueView extends React.Component {
 
   render() {
     const { queue } = this.props;
-    const lastEntry = queue && queue[queue.length-1];
+    const lastEntry = queue && queue[queue.length - 1];
     const timeNow = new Date().getTime();
     const updateTime = lastEntry &&
-                       ((timeNow - lastEntry.loggedAt) / 60000) | 0;
+                       ((timeNow - lastEntry.loggedAt) / 60000).toFixed(0);
 
     return (
-      <Box>
-        {!queue ?
-         <Text>No queues yet.</Text>
-         :
-         <Box
-           display="flex"
-           flexDirection="column"
-           justifyContent="center"
-           alignItems="center"
-           paddingVertical={0.1}
-           paddingHorizontal={0.1}
-           backgroundColor={makeColor(lastEntry.value)}
-           border={true}
-           borderColor={makeColor(lastEntry.value)}
-           borderRadius={40}
-         >
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        paddingVertical={0.1}
+        paddingHorizontal={0.1}
+        backgroundColor="black"
+        border
+        borderWidth={2}
+        borderColor={lastEntry && makeColor(lastEntry.value)}
+        borderRadius={5}
+      >
+        {queue ?
+         <Box>
            <Box marginVertical={0}>
-             <Text color="black" size={0}>
+             <Text color="white" bold size={0}>
                {this.genValueText(lastEntry.value)}
              </Text>
-             <Text color="black" size={0}>
+             <Text color="white" bold size={0}>
                {this.genUnitText(lastEntry.value)}
              </Text>
            </Box>
            <Box marginTop={-0.45}>
-             <Text color="black" size={-2}>
+             <Text bold color="white" size={-2}>
                {this.genUpdateText(updateTime)}
              </Text>
            </Box>
          </Box>
+         :
+         <Text>No queues yet.</Text>
         }
       </Box>
     );
